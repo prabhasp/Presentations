d9d62e8bf8e2b1dc33719e68b303f74c57a80673
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2013-06-15 16:32:20 +0200

    electricity + health data

--- prabhas/energy-summaries/data/MakeSectorData.R
+++ prabhas/energy-summaries/data/MakeSectorData.R
@@ -1,19 +1,50 @@
 setwd("~/Code/Presentations/prabhas/energy-summaries/data/")
 library(plyr)
-education <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Education_661_ALL_FACILITY_INDICATORS.csv")
-education <- subset(education, select=c("X_lga_id", "power_access", "mylga", "mylga_state", "mylga_zone",
-               "grid_proximity", "power_sources.generator", "power_sources.solar"))
-education <- rename(education,c("X_lga_id" = "lga_id", "mylga"="lga", "mylga_state"="state", "mylga_zone"="zone", "power_access"="functional"))
+library(stringr)
+e6 <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Education_661_ALL_FACILITY_INDICATORS.csv")
+e1 <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Education_113_ALL_FACILITY_INDICATORS.csv")
+ep <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Education_Pilot_ALL_FACILITY_INDICATORS.csv")
+e6 <- subset(e6, select=c("X_lga_id", "power_access",
+                          "power_sources.grid", "power_sources.generator", "power_sources.solar_system", 
+                          "generator_funct_yn", "solar_funct_yn", "grid_funct_yn"))
+e1 <- subset(e1, select=c("X_lga_id", "power_access",
+                          "power_grid_connection", "power_generator", "power_solar_system",
+                          "generator_funct_yn", "solar_funct_yn", "grid_funct_yn"))
+ep[,c('power_access', 'generator_funct_yn', 'solar_funct_yn', 'grid_funct_yn')] <- NA
+ep <- subset(ep, select=c("X_lga_id", "power_access",
+                          "power_grid_connection", "power_generator", "power_solar_system",
+                          "generator_funct_yn", "solar_funct_yn", "grid_funct_yn"))
+### WARNING: MAKE SURE ORDER IS THE SAME!
+std_names <- c("lga_id", "functional_power", "grid", "genset", "solar",
+               "generator_functional", "solar_functional", "grid_functional")
+names(e1) <- std_names
+names(e6) <- std_names
+names(ep) <- std_names
+education <- rbind(e1, e6, ep)
 education$sector <- "EDUCATION"
 
-write.csv(education, "EducationElectricityData.csv")
+write.csv(education, "EducationElectricityData.csv", row.names=F)
 
 
-health <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Health_661_ALL_FACILITY_INDICATORS.csv")
-health <- subset(health, select=c("X_lga_id", "power_access_and_functional", "mylga", "mylga_state", "mylga_zone",
-           "not_for_private_1.grid_proximity", "health$not_for_private_1.power_sources.generator",
-            "health$not_for_private_1.power_sources.solar"))
-health <- rename(health,c("X_lga_id" = "lga_id", "mylga"="lga", "mylga_state"="state", "mylga_zone"="zone", 
-                          "not_for_private_1.grid_proximity"="grid_proximity", "power_access_and_functional"="functional"))
+h6 <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Health_661_ALL_FACILITY_INDICATORS.csv")
+h1 <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Health_113_ALL_FACILITY_INDICATORS.csv")
+hp <- read.csv("~/Dropbox/Nigeria/Nigeria 661 Baseline Data Cleaning/in_process_data/nmis/Health_Pilot_ALL_FACILITY_INDICATORS.csv")
+names(h6) <- str_replace(names(h6), "not_for_private_[0-9].", "")
+h6 <- subset(h6, select=c("X_lga_id", "power_access_and_functional",
+                          "power_sources.grid", "power_sources.generator", "power_sources.solar", 
+                          "generator_funct_yn", "solar_funct_yn", "grid_funct_yn"))
+h1 <- subset(h1, select=c("X_lga_id", "power_access_and_functional",
+                          "power_sources_grid", "power_sources_generator", "power_sources_solar",
+                          "generator_funct_yn", "solar_funct_yn", "grid_funct_yn"))
+hp <- subset(hp, select=c("X_lga_id", "power_access_and_functional",
+                          "power_sources_grid", "power_sources_generator", "power_sources_solar",
+                          "generator_funct_yn", "solar_funct_yn", "grid_funct_yn"))
+### WARNING: WATCH THE ORDER!
+std_names <- c("lga_id", "functional_power", "grid", "genset", "solar",
+               "generator_functional", "solar_functional", "grid_functional")
+names(h1) <- std_names
+names(h6) <- std_names
+names(hp) <- std_names
+health <- rbind(h1, hp, h6)
 health$sector <- "HEALTH"
-write.csv(health, "HealthElectricityData.csv")
+write.csv(health, "HealthElectricityData.csv", row.names=F)
